cbda351b28759ee187dcdef20324b1d4a1856b8b
Rafael <cam73@yandex.ru>
2013-09-08 02:24:15 +0400

    first commit

---
+++ www/web/index.php
@@ -0,0 +1,21 @@
+<?php header('Content-type: text/html; charset=utf-8'); ?><!DOCTYPE html>
+<html lang="ru" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8" />
+    <title>Официальный сайт ТСЖ Пуршево</title>
+    <link rel="stylesheet" type="text/css" href="/css/main.css" />
+</head>
+<body>
+    <div class="maindiv">
+        <div class="secondiv">
+            <div>
+                <strong>Официальный сайт ТСЖ &laquo;Пуршево&raquo;</strong><br/><br/>
+
+                Сайт находится на стадии разработки<br/><br/>
+
+                <a href="http://tsg-system.ru/cabinet">Перейти в личный кабинет собственника</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
